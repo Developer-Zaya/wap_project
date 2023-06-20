da26a4392ecac8246c65e81692bf9ac2f8102395
Developer-Zaya <Ariunzaya972@gmail.com>
2023-06-20 16:34:26 -0500

    add product

--- shoppingcart-server/controllers/shoppingCartController.js
+++ shoppingcart-server/controllers/shoppingCartController.js
@@ -1,12 +1,25 @@
 const ShoppingCart = require("../models/shoppingCart");
+const User = require("../models/users");
 
 exports.getShoppingCarts =(req, res)=>{
     res.status(200).json(ShoppingCart.getShoppingCart());
 }
 exports.addToCart =(req,res)=>{
-    const { products } = res.body;
+    
+    const products = req.body;
     if(!products){
         res.status(403).json({error:"please include products"})
     }
-    res.status(200).json(ShoppingCart.addToShoppingCart(products));
+    const user = User.auth(req.body.user);
+    
+    if (!user.cart){
+        const newCart = new ShoppingCart(user.username);
+        user.cart = newCart;
+        console.log(user)
+    }
+    console.log(user.cart.product,products)
+    pro = user.cart.addToShoppingCart(products)
+    
+    res.status(200).json({ message: "Products added to the shopping cart" });
+    
 }
