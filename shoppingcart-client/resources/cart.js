4ef25c2a0dd1da7f34fc6c53b94ba2dd034f22c9
Developer-Zaya <Ariunzaya972@gmail.com>
2023-06-20 00:24:22 -0500

    client

--- shoppingcart-client/resources/cart.js
+++ shoppingcart-client/resources/cart.js
@@ -1,15 +1,6 @@
 window.onload = function () {
     getProducts();
     const token = sessionStorage.getItem("data")
-
-    document.getElementById('product-table').addEventListener('click', function (event) {
-        event.preventDefault();
-        const target = event.target;
-        if (target.matches('.btn')) {
-            login();
-        }
-    });
-
     document.getElementById('login-user').innerText=token.split(",")[0];
 
     document.getElementById('logout-btn').addEventListener('click', function (event) {
@@ -21,6 +12,52 @@ async function getProducts() {
     const token = sessionStorage.getItem("data")
     let products = await fetch('http://localhost:3000/products/').then(response => response.json());
     renderProduct(products)
+}
+
+let cart = []
+
+async function addToCart(id){
+    let product = await fetch('http://localhost:3000/products/'+id).then(response => response.json());
+    console.log(product);
+}
+
+function renderCart(prod){
+    let table = document.createElement('table');
+    table.classList.add('table', 'table-bordered');
+    let thead = document.createElement('thead');
+    let tr = document.createElement('tr');
+    let headers = ['Name', 'Price', 'Total', 'Quantity'];
+    headers.forEach(headerText => {
+        let th = document.createElement('th');
+        th.classList.add('text-center');
+        th.textContent = headerText;
+        tr.appendChild(th);
+    });
+    thead.appendChild(tr);
+    table.appendChild(thead);
+    let tbody = document.createElement('tbody');
+
+    prod.forEach(product => {
+        let tr = document.createElement('tr');
+
+        let nameTd = document.createElement('td');
+        nameTd.textContent = product.name;
+        tr.appendChild(nameTd);
+
+        let priceTd = document.createElement('td');
+        priceTd.textContent = product.price;
+        tr.appendChild(priceTd);
+
+        let total = document.createElement('td');
+        total.textContent = product.total;
+        tr.appendChild(total);
+
+        tbody.appendChild(tr);
+    });
+
+    table.appendChild(tbody);
+    let container = document.getElementById('my-cart');
+    container.appendChild(table);
 }
 
 function renderProduct(prod) {
@@ -67,8 +104,8 @@ function renderProduct(prod) {
 
         let actionsTd = document.createElement('td');
         let button = document.createElement('button');
-        button.id = `${product.name.toLowerCase()}-btn`;
-        button.classList.add('btn', 'btn-outline-success', 'fw-bold', 'd-inline');
+        button.classList.add('btn', 'btn-outline-success', 'fw-bold', 'd-inline', 'order');
+        button.onclick = ()=>{addToCart(product.id)};
         let icon = document.createElement('i');
         icon.classList.add('fa', 'fa-shopping-cart');
         button.appendChild(icon);
